3d6e5c698ae1ce1bc2c98676938d939fea7232ac
PedroASB <pedroasbelo@gmail.com>
2024-05-04 15:04:37 -0300

    feat(seminario1): versão inicial do Graham Scan

--- src/seminario1/main.cpp
+++ src/seminario1/main.cpp
@@ -1,73 +1,98 @@
-#include <gl/gl.h>
-#include <gl/glu.h>
 #include <GL/glut.h>
-#include <list>
-#include <stdio.h>
+#include <algorithm>
 #include <cmath>
+#include "graham_scan.hpp"
 #include <vector>
-#include "utils.hpp"
 
-// g++ -Wall ./*.cpp -I"../../include/freeglut/include" -L"../../include/freeglut/lib" -lfreeglut -lopengl32  -lglu32 -lm -o main
+// g++ -Wall ./*.cpp -I"../../include/freeglut/include" -L"../../include/freeglut/lib" -lfreeglut -lopengl32  -lglu32 -lm -o ./out/main
 
-#define WIDTH 600
-#define HEIGHT 600
+#define WIDTH 500
+#define HEIGHT 500
+#define NUM_POINTS 5
 
-std::vector<Point *> points;
+void draw_axes() {
+    glColor3f(0.0, 0.0, 0.0);
+    glPointSize(1.5f);
+    for (int i = (int)-(WIDTH / 2); i <= (int)(WIDTH / 2); i += 5) {
+        if (std::abs(i) % 25 == 0) {
+            glBegin(GL_POINTS);
+            glVertex2i(i, 0);
+            glEnd();
+        }
+    }
+    for (int i = (int)-(HEIGHT / 2); i <= (int)(HEIGHT / 2); i += 5) {
+        if (std::abs(i) % 25 == 0) {
+            glBegin(GL_POINTS);
+            glVertex2i(0, i);
+            glEnd();
+        }
+    }
+    glFlush();
+}
 
-// void mouse_handler(int button, int state, int _x, int _y) {
-//     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-//         points.push_back(new Point{(GLuint)_x, (GLuint)_y});
-//     }
 
-//     if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-//         if (!points.empty()) {
-//             std::list<Point *>::iterator it;
-//             for (it = points.begin(); it != points.end(); ++it) {
-//                 if (distanceSqrd((float)(*(*it)).x, (float)(*(*it)).y, (float)_x, (float)_y) <= 64.f) {
-//                     delete *it;
-//                     points.erase(it);
-//                     break;
-//                 }
-//             }
-//         }
-//     }
-// }
+void draw_convex_hull() {
 
-void reshape_handler(int w, int h) {
-    glutReshapeWindow(WIDTH, HEIGHT);
 }
 
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
+    vector<Point> result_points;
+    Point points[NUM_POINTS];
 
-    glPointSize(15); // Tamanho do ponto
+    // Desenha os eixos x e y
+    draw_axes();
+
+    points[0].x = 30;
+    points[0].y = 40;
+
+    points[1].x = 60;
+    points[1].y = 40;
+
+    points[2].x = 50;
+    points[2].y = 80;
+
+    points[3].x = 100;
+    points[3].y = 10;
+
+    points[4].x = 120;
+    points[4].y = 130;
+
+    glPointSize(7); // Tamanho do ponto
     glBegin(GL_POINTS);
-        glColor3f(1.0, 0.0, 0.0); // Cor do ponto (vermelho)
-        for (auto p : points) {
-            glVertex2i((*p).x, HEIGHT - (*p).y);
+        glColor3f(0.0, 0.0, 1.0); // Cor do ponto
+        for (int i = 0; i < NUM_POINTS; i++) {
+            glVertex2i(points[i].x, points[i].y);
         }
     glEnd();
 
+    result_points = convexHull(points, NUM_POINTS);
+
+    glPointSize(7); // Tamanho do ponto
+    glBegin(GL_POINTS);
+        glColor3f(1.0, 0.0, 0.0); // Cor do ponto
+        for (auto p : result_points) {
+            glVertex2i(p.x, p.y);
+        }
+    glEnd();
+    
     glFlush();
 }
 
-// ============= Main =============
+void init(void) {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-(WIDTH / 2), (WIDTH / 2), -(HEIGHT / 2), (HEIGHT / 2));
+}
+
 int main(int argc, char **argv) {
-
-    printf("teste\n");
-
-    // glutInit(&argc, argv);
-    // glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    // glutInitWindowSize(WIDTH, HEIGHT);
-    // glutInitWindowPosition(100, 100);
-    // glutCreateWindow("Grupo 1 - Fecho convexo 2D");
-    // glClearColor(1.0, 1.0, 1.0, 0.0);
-    // glShadeModel(GL_FLAT);
-    // gluOrtho2D(0, WIDTH, 0, HEIGHT); // Define o sistema de coordenadas da janela
-    // glViewport(0, 0, WIDTH, HEIGHT);
-    // glutMouseFunc(mouse_handler);
-    // glutDisplayFunc(display);
-    // glutReshapeFunc(reshape_handler);
-    // glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50, 100);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("Retas Otimizadas");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
